db45a6d3e3900c96495735a353fba64508437f9a
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-07-06 01:21:31 +0700

    Update alive.py

--- KynanRobot/modules/alive.py
+++ KynanRobot/modules/alive.py
@@ -18,7 +18,7 @@ async def awake(_, message: Message):
     TEXT += "**ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴋᴜ ᴅɪsɪɴɪ**"
     BUTTON = [
         [
-            InlineKeyboardButton("ʜᴇʟᴘ", url=f"https://t.me/ZoidsXRobot_bot?start=help"),
+            InlineKeyboardButton("ʜᴇʟᴘ", url=f"https://t.me/AliceRobot?start=help"),
             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/ZOIDsSupport"),
         ],
         [
